4d4636b07bb56d3c59b0099e13feb9c57f9d2ce9
Alexandra O'Mahony <alexandra.omahony1@gmail.com>
2020-03-05 17:40:58 +0000

    Cards now looks as per wireframe for desktop and mobile
    closes #8

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -2,19 +2,22 @@ import React from 'react';
 import Button from './button/Button';
 import { ReactComponent as BestTag } from '../../assets/Allcats_badge.svg';
 import styled from 'styled-components';
+import { P, H1 } from '../styled/StyledComponents';
 
 const Article = styled.article`
   border: 1px solid black;
   width: 20vw;
+  max-width: 250px;
   margin: var(--space-s);
   padding: var(--space-xs);
   background-color: white;
-
   @media (max-width: 768px) {
     width: 40%;
   }
 `;
 const ImageContainer = styled.div`
+  display: flex;
+  align-items: flex-start;
   background-image: url(${props => props.imageUrl});
   background-size: cover;
   background-position: center;
@@ -23,10 +26,29 @@ const ImageContainer = styled.div`
   align: center;
 `;
 const CardSection = styled.section`
+  box-sizing: border-box;
   display: flex;
   flex-flow: row wrap;
   justify-content: center;
   width: 100%;
+  padding: 0 10vw;
+
+  @media (max-width: 768px) {
+    padding: 0;
+  }
+`;
+
+const ContentContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+  padding-top: var(--space-s);
+  background-image: url(${props => props.imageUrl});
+  background-size: cover;
+  background-position: center;
+  height: ${props => props.height || '10rem'};
+  width: 100%;
+  align: center;
 `;
 
 const Card = ({ cats }) => {
@@ -38,9 +60,10 @@ const Card = ({ cats }) => {
               <ImageContainer imageUrl={cat.image} alt={cat.name}>
                 {cat.tags[0] === 'best' ? <BestTag></BestTag> : null}
               </ImageContainer>
-              <h1>{cat.name}</h1>
-
-              <p>{cat.description}</p>
+              <ContentContainer>
+                <H1>{cat.name}</H1>
+                <P>{cat.description}</P>
+              </ContentContainer>
               <Button></Button>
             </Article>
           ))
--- src/components/card/button/Button.js
+++ src/components/card/button/Button.js
@@ -4,6 +4,10 @@ import styled from 'styled-components';
 const TakeHome = styled.button`
   background-color: var(--primary-colour);
   cursor: pointer;
+  border: none;
+  padding: var(--space-s) var(--space-m);
+  color: white;
+  width: 100%;
 `;
 const Button = () => {
   return <TakeHome>TAKE HOME</TakeHome>;
